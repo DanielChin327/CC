9eb8480b83708915e0f9b911b8d60d443bf34cd0
Daniel Chin <DanChin327@gmail.com>
2024-12-23 14:47:23 -0700

    installed gsap

---
+++ scripts/animations.js
@@ -0,0 +1,5 @@
+// scripts/animations.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Animate the box to move 300px to the right over 1 second
+  gsap.to("#animated-box", { y: 200, duration: 1 });
+});
